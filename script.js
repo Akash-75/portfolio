53dfad2d2cc01b38dc75c849b3d4574df53d8255
Akash-75 <132268764+Akash-75@users.noreply.github.com>
2023-05-02 00:40:44 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// -------------menu icons--------------
+
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
